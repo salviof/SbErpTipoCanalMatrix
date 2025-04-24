02589a9cd002cb8797ec1f147f99be92f596fccc
Sálvio Furbino <salviof@gmail.com>
2025-04-23 22:43:31 -0300

    Atualizacao automática

---
+++ src/main/java/br/org/coletivoJava/fw/erp/implementacao/transportecomunicacao/MsgDisparoMatriximpl.java
@@ -0,0 +1,54 @@
+package br.org.coletivoJava.fw.erp.implementacao.transportecomunicacao;
+
+import br.org.coletivoJava.fw.api.erp.chat.ERPChat;
+import br.org.coletivoJava.fw.api.erp.chat.ErroConexaoServicoChat;
+import br.org.coletivoJava.fw.api.erp.chat.model.ItfUsuarioChat;
+import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfDialogo;
+import com.super_bits.modulosSB.SBCore.integracao.libRestClient.api.erp.repositorioLinkEntidades.RepositorioLinkEntidadesGenerico;
+import br.org.coletivoJava.fw.api.erp.transportecomunicacao.MsgDisparoMatrix;
+import br.org.coletivoJava.fw.erp.implementacao.chat.ChatMatrixOrgimpl;
+import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.coletivojava.fw.api.tratamentoErros.FabErro;
+
+@MsgDisparoMatrix
+public class MsgDisparoMatriximpl extends RepositorioLinkEntidadesGenerico
+        implements
+        com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfDisparoComunicacao {
+
+    private static ChatMatrixOrgimpl chatMatrix = (ChatMatrixOrgimpl) ERPChat.MATRIX_ORG.getImplementacaoDoContexto();
+
+    @Override
+    public String dispararInicioComunicacao(ItfDialogo pDialogo) {
+
+        String email = pDialogo.getDestinatario().getUsuario().getEmail();
+        ItfUsuarioChat usuario = null;
+        try {
+            usuario = chatMatrix.getUsuarioByEmail(email);
+        } catch (ErroConexaoServicoChat ex) {
+            SBCore.RelatarErro(FabErro.SOLICITAR_REPARO, "Falha buscando usuário por e-mail" + email, ex);
+            return null;
+        }
+
+        if (usuario != null) {
+            try {
+                String reciboEntrega = chatMatrix.enviarDirect(usuario.getCodigoUsuario(), pDialogo.getAssunto() + " " + pDialogo.getMensagem());
+                return reciboEntrega;
+            } catch (ErroConexaoServicoChat ex) {
+                SBCore.RelatarErro(FabErro.SOLICITAR_REPARO, email, ex);
+            }
+        } else {
+            System.out.println(email + " não encontrado no serviço Matrix");
+            return null;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void dispararRespostaComunicacao(
+            ItfDialogo itfDialogo) {
+
+    }
+}
--- src/test/java/br/org/coletivoJava/fw/erp/implementacao/transportecomunicacao/MsgDisparoMatriximplTest.java
+++ src/test/java/br/org/coletivoJava/fw/erp/implementacao/transportecomunicacao/MsgDisparoMatriximplTest.java
@@ -8,7 +8,9 @@ import br.org.coletivoJava.fw.erp.implementacao.transportecomunicacao.dados.Usua
 import br.org.coletivojava.erp.comunicacao.transporte.ERPTipoCanalComunicacao;
 import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
 import com.super_bits.modulosSB.SBCore.modulos.comunicacao.FabTipoComunicacao;
-import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.ItensGenericos.basico.UsuarioSistemaRoot;
+import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfDialogo;
+import com.super_bits.modulosSB.SBCore.modulos.servicosCore.ErroAcessandoCanalComunicacao;
+import org.coletivojava.fw.api.tratamentoErros.FabErro;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,45 +18,29 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import testesFW.TesteJUnitBasicoSemAcoes;
-import testesFW.TesteJUnitBasicoSemPersistencia;
-import testesFW.TesteJunitSBPersistencia;
-import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfDialogo;
-import com.super_bits.modulosSB.SBCore.modulos.servicosCore.ErroAcessandoCanalComunicacao;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.coletivojava.fw.api.tratamentoErros.FabErro;
 
 /**
  *
  * @author salvio
  */
-public class MsgDisparoEmailimplTest extends TesteJUnitBasicoSemAcoes {
+public class MsgDisparoMatriximplTest extends TesteJUnitBasicoSemAcoes {
 
-    public MsgDisparoEmailimplTest() {
+    public MsgDisparoMatriximplTest() {
     }
 
     /**
-     * Test of dispararInicioComunicacao method, of class MsgDisparoEmailimpl.
+     * Test of dispararInicioComunicacao method, of class MsgDisparoMatriximpl.
      */
     @Test
     public void testDispararInicioComunicacao() {
-
         UsuarioTestesComunicacao usuarionotificacao = new UsuarioTestesComunicacao();
         ItfDialogo dialogo = SBCore.getServicoComunicacao().gerarComunicacaoSistema_Usuario(FabTipoComunicacao.NOTIFICAR,
                 usuarionotificacao, "Apenas teste", "Mensagem do teste de notificação via email");
         try {
-            SBCore.getServicoComunicacao().dispararComunicacao(dialogo, ERPTipoCanalComunicacao.EMAIL);
+            SBCore.getServicoComunicacao().dispararComunicacao(dialogo, ERPTipoCanalComunicacao.MATRIX);
         } catch (ErroAcessandoCanalComunicacao ex) {
             SBCore.RelatarErro(FabErro.SOLICITAR_REPARO, "Falha disparando comunicado pelo canal de email", ex);
         }
-    }
-
-    /**
-     * Test of dispararRespostaComunicacao method, of class MsgDisparoEmailimpl.
-     */
-    @Test
-    public void testDispararRespostaComunicacao() {
-
     }
 
 }
